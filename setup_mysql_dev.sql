4e3fa2d52a66cad8b1aca0fb09776490bae16e7a
Motaz-Mukhtar <motazksa2@gmail.com>
2023-01-04 08:48:01 -0800

    Modify setup_mysql_dev.sql file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,10 +7,10 @@ CREATE USER
 	IDENTIFIED BY 'hbnb_dev_pwd';
 
 GRANT ALL PRIVILEGES
-	ON 'hbnb_dev_db'.*
+	ON `hbnb_dev_db`.*
 	TO 'hbnb_dev'@'localhost';
 GRANT SELECT
-	ON 'performance_schema'.*
+	ON `performance_schema`.*
 	TO 'hbnb_dev'@'localhost';
 
 FLUSH PRIVILEGES;
