37f50546d1c95d4ef89c6c53cb59258ce1e48123
Motaz-Mukhtar <motazksa2@gmail.com>
2023-02-05 22:32:55 -0800

    Add 8-cities_by_staes.py and 8-cities_by_states.html files

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Start Flask Web Application """
-from flask import Flask, render_template
-from ..models import storage
+from flask import Flask
+from flask import render_template
+from models import storage
 
 
 app = Flask(__name__)
@@ -14,11 +15,10 @@ def states_list():
     return render_template('7-states_list.html', name=states)
 
 
-@app.route('/cities_by_states', strict_slashes=False):
-    def cities_by_states():
-        states = storage.all("State")
-        cities = storage.all("City")
-        return render_template('8-cities_by_states.html', states=states, cities=cities)
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    states = storage.all("State")
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
@@ -28,4 +28,4 @@ def teardown(se):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port="5000")
+    app.run(host='0.0.0.0', port="5000", debug=1)
