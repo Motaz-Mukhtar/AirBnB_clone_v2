1125bf1f04f5017c3e65c3e0416da315ebaf6463
Motaz-Mukhtar <motazksa2@gmail.com>
2023-02-06 23:16:48 -0800

    hotfix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,21 +10,21 @@ app = Flask(__name__)
 
 @app.route('/states', strict_slashes=False)
 def states():
-    """ List all State objects """
+    """List all State objects """
     states = storage.all("State")
+    if len(storage.all("State")) == 0:
+        states = 0
     return render_template("9-states.html", states=states)
 
 
 @app.route("/states/<id>", strict_slashes=False)
 def state_id(id):
     """ list City objects liked to the State """
-    state = storage.all("State").values()
-    for i in state:
-        if i.id == id:
-            state = i
-            break
-        state = 0
-    return render_template("9-states.html", state=state)
+    states = storage.all("State").values()
+    for state in states:
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
@@ -34,4 +34,4 @@ def teardown(se):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port="5000")
+    app.run(host='0.0.0.0', port="5000", debug=1)
